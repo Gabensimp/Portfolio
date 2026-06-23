2c2603fcb50878a3d5328f74a6c1182e196d9be9
Rocsan Odi <Gabensimp@gmail.com>
2026-06-24 05:45:51 +0800

    commit

---
+++ Portfolio.js
@@ -0,0 +1,86 @@
+// ------------------ About Tabs ------------------
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// ------------------ Mobile Menu ------------------
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// ------------------ Contact Form ------------------
+const scriptURL =
+    "https://script.google.com/macros/s/AKfycbyHnCEil1EQStip1Kfek-DM0p4EbljTfZcJCgYZSYQnCfjcXExnsJ97W2t1gRmrm4wvCA/exec";
+
+const form = document.forms["submit-to-google-sheet"];
+const ms = document.getElementById("ms");
+
+let canSubmit = true;
+
+if (form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        if (!canSubmit) {
+            ms.innerHTML =
+                "Please wait 10 seconds before sending another message.";
+
+            setTimeout(() => {
+                ms.innerHTML = "";
+            }, 3000);
+
+            return;
+        }
+
+        canSubmit = false;
+
+        fetch(scriptURL, {
+            method: "POST",
+            body: new FormData(form),
+        })
+            .then((response) => {
+                ms.innerHTML = "Message has been sent!";
+
+                setTimeout(() => {
+                    ms.innerHTML = "";
+                }, 5000);
+
+                form.reset();
+
+                setTimeout(() => {
+                    canSubmit = true;
+                }, 10000);
+            })
+            .catch((error) => {
+                console.error("Error!", error.message);
+
+                ms.innerHTML = "There was an error sending your message.";
+
+                setTimeout(() => {
+                    ms.innerHTML = "";
+                }, 5000);
+
+                setTimeout(() => {
+                    canSubmit = true;
+                }, 10000);
+            });
+    });
+}
